5dceafe9e60fbcc973e7c8420ffc501988916a14
Simon Sun <sunmingyu.97@gmail.com>
2021-02-04 19:41:24 +0000

    C language

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print string of a-z
+ *
+ * Return: 0
+ *
+ *
+*/
+int main(void)
+{
+	char c;
+	int i;
+
+	c = 'a';
+	for (i = 0; i < 26; i++)
+	{
+	putchar(c);
+	c++;
+	}
+	putchar('\n');
+	return (0);
+
+}
+
