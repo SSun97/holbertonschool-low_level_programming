75c1dfa46c50e856e082164ba422593530e2ddfa
Simon Sun <sunmingyu.97@gmail.com>
2021-02-04 23:27:14 +0000

    C language

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *main - print 00 - 99 with comma
+ *
+ *Return: 0
+*/
+int main(void)
+{
+	int i;
+	int j;
+	i = 48;
+
+	while (i < 58)
+	{
+		j = 48;
+		while (j < 58)
+		{
+			putchar(i);
+			putchar(j);
+			if (i == 57 && j == 57)
+				break;
+			j++;
+				putchar(',');
+				putchar(' ');
+		}
+	i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
