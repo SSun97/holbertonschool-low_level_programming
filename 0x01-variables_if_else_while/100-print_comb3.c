06f8886d7ae3acb6272b5206c3ab206cf23bf9e8
Simon Sun <sunmingyu.97@gmail.com>
2021-02-05 01:22:21 +0000

    C language

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+/**
+ *main - print 00 - 99 with comma
+ *
+ *Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+
+	i = 48;
+
+	while (i < 57)
+	{
+		j = 48;
+		while (j < 58)
+		{
+			if (i == j)
+			{
+				j++;
+				continue;
+			}
+			if (i != 48 && j == 48)
+			{
+				j++;
+				continue;
+			}
+
+			putchar(i);
+			putchar(j);
+			if (i == 56 && j == 57)
+				break;
+			j++;
+				putchar(',');
+				putchar(' ');
+		}
+	i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
