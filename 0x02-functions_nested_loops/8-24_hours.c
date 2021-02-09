6a8fcd4335e611679373ca86bae9e96590df744c
Simon Sun <sunmingyu.97@gmail.com>
2021-02-09 13:30:10 +0000

    first push

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * jack_bauer - print time
+*/
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 24; i++)
+		for (j = 0; j < 60; j++)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+		}
+}
