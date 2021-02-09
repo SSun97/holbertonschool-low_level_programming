942595ba07f4313456c40e0479c5c593ac52e7e1
Simon Sun <sunmingyu.97@gmail.com>
2021-02-09 14:41:22 +0000

    first push

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/* times_table - Print times table */
+
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (j > 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (i * j < 10)
+			{
+				_putchar(' ');
+				_putchar(i * j + '0');
+			}
+			else
+			{
+			_putchar((i * j) / 10 + '0');
+			_putchar((i * j) % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
