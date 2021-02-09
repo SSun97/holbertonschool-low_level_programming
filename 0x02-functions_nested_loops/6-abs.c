b139cc0b6bf37405358302ba5124709d79478b23
Simon Sun <sunmingyu.97@gmail.com>
2021-02-09 11:44:29 +0000

    first push

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+/**
+ * _abs - get the absolute value of int n
+ * @n: type int
+ *
+ * Return: n if n is larger than 0, otherwise -n
+ *
+*/
+int _abs(int n)
+{	if (n >=  0)
+	return (n);
+	else
+	return (-n);
+}
