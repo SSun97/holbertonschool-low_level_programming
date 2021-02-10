ab799e0dd7347fd790fd1d31f16ef067bb78c675
Simon Sun <sunmingyu.97@gmail.com>
2021-02-10 03:50:45 +0000

    first push

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - print sum of numbers that are multiple of 3 or 5
+ *
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+	int sum;
+
+	sum = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum = sum + i;
+			i++;
+		}
+		else
+		{
+			i++;
+			continue;
+		}
+
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
