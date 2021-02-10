65816a0193321e203e63e1d3101486ad8b646907
Simon Sun <sunmingyu.97@gmail.com>
2021-02-10 05:24:58 +0000

    first push

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - print the first 50 fibonacci number
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	long int i, a1 = 1, a2 = 2, c;
+
+	for (i = 0; i < 26; ++i)
+	{
+		if (a1 > 4000000)
+			break;
+		else if (a1 % 2 == 0)
+		{
+			printf("%lu", a1);
+			if (i < 25)
+			printf(", ");
+		}
+		c = a1 + a2;
+		a1 = a2;
+		a2 = c;
+	}
+	printf("\n");
+
+
+	return (0);
+}
