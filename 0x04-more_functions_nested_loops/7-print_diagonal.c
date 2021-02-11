ac376853e742cbb8d5c594ef17babf45efc22c05
Simon Sun <sunmingyu.97@gmail.com>
2021-02-11 18:04:46 +0000

    first push

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * print_diagonal - pring n times of "_"
+ * @n: type int
+ *
+*/
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	_putchar('\n');
+
+}
