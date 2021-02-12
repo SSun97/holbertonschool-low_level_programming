b2690e3bc5dda9373c3177dc5c660757f76be15a
Simon Sun <sunmingyu.97@gmail.com>
2021-02-12 00:24:22 +0000

    first push

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_number - print int through putchar
+ * @n: type int
+ *
+*/
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
+}
