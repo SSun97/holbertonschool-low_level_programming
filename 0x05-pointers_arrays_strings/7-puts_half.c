19837fc55fd352b5cbc245a53a6d6403ca384eb9
Simon Sun <sunmingyu.97@gmail.com>
2021-02-16 23:04:09 +0000

    change the code for calculating string length

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,13 +7,20 @@
 void puts_half(char *str)
 {
 	char *p = str;
+	int c;
+	int i = 0;
 
-	while (*(p + 1) != '\0')
-		p++;
-	while (p >= str)
+	while (*p != '\0')
 	{
-		_putchar(*(str + (p - str) / 2 + 1));
-		str += 2;
+		p++;
+		i++;
+	}
+	c = i / 2 + 1;
+
+	while (*(str + c) != '\0')
+	{
+		_putchar(*(str + c));
+		c += 1;
 	}
 	_putchar('\n');
 
