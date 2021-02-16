0e6a8abe03a6d3692a29fd8b10187e9888bdcbea
Simon Sun <sunmingyu.97@gmail.com>
2021-02-16 06:17:01 +0000

    first push

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+/**
+ * _strcpy - copy the string
+ * @dest: type char*
+ * @src: type char *
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; *(src + i) != '\0'; i++)
+		*(dest + i) = *(src + i);
+	*(dest + i + 1) = '\0';
+
+
+
+	return (dest);
+}
+
+
+
