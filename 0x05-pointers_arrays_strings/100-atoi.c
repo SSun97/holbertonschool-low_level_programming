a09eb4f5680fe2cb22b1dae1a34c9624f7dd16b9
Simon Sun <sunmingyu.97@gmail.com>
2021-02-17 03:13:55 +0000

    solve the space make it jupm out issue

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+/**
+ * _atoi - check the code for Holberton School students.
+ * @s: type pointer
+ * Return: tyoe int.
+ */
+
+
+int _atoi(char *s)
+{
+	char *p = s;
+	int i = 0;
+	unsigned int digit;
+	int sign = 1;
+
+	while (*p < '0' || *p > '9')
+	{
+		if (*p == '-')
+		{
+			sign = sign * (-1);
+		}
+		p += 1;
+	}
+
+	for ( ; ; p += 1)
+	{
+		digit = *p - '0';
+		if (digit > 9)
+			break;
+		i = 10 * i + digit;
+
+	}
+	if (sign == -1)
+	{
+		return (-i);
+
+	}
+	return (i);
