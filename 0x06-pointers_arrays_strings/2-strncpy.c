b1c592c587d70c7fac84e2fefd2a43ae78b55f87
Simon Sun <sunmingyu.97@gmail.com>
2021-02-18 21:59:02 +0000

    change the syntax

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,26 +7,13 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *x;
-	char *y;
-	char *z;
-	int i = 0;
-	int j;
+	int i;
 
-	x = dest;
-	y = src;
-	z = src;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
 
-	for (j = 0; *z != '\0'; j++)
-		z++;
-
-	while (*y != '\0' && i < n)
-	{
-		*x++ = *y++;
-		i++;
-	}
-	if (j <= n)
-		*x = '\0';
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 
 	return (dest);
 
