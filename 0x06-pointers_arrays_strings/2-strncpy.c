e47556f326d87d1960688f0afa8354253fc4248f
Simon Sun <sunmingyu.97@gmail.com>
2021-02-18 20:08:33 +0000

    first push

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,33 @@
+/**
+ * _strncpy - copy string
+ * @dest: type char*
+ * @src: type char*
+ * @n: type int
+ * Return: type char*.
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *x;
+	char *y;
+	char *z;
+	int i = 0;
+	int j;
+
+	x = dest;
+	y = src;
+	z = src;
+
+	for (j = 0; *z != '\0'; j++)
+		z++;
+
+	while (*y != '\0' && i < n)
+	{
+		*x++ = *y++;
+		i++;
+	}
+	if (j < n)
+		*x = '\0';
+
+	return (dest);
+
+}
