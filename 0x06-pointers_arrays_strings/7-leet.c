f5733dfce1eaf624adde32ae40f6889d5d44cc2c
Simon Sun <sunmingyu.97@gmail.com>
2021-02-19 05:23:56 +0000

    first push

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+/**
+* leet - output string into leet
+* @a: type char *
+* Return: char.
+*/
+
+
+char *leet(char *a)
+{
+	char e[11] = { 'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L' };
+	char l[10] = { '4', '3', '0', '7', '1', '4', '3', '0', '7', '1' };
+	int i;
+	char *p;
+
+	p = a;
+
+	while (*p != '\0')
+	{
+
+		for (i = 0; i < 10; i++)
+		{
+			if (*p == e[i])
+				*p = l[i];
+		}
+		p++;
+	}
+	return (a);
+}
