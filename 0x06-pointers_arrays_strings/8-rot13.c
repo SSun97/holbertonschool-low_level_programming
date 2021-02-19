e7db176a5c1ee5538396ffc1db28d177b0af2f1f
Simon Sun <sunmingyu.97@gmail.com>
2021-02-19 05:58:00 +0000

    first push

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,42 @@
+/**
+* rot13 - output string into rot13
+* @a: type char *
+* Return: char.
+*/
+
+
+char *rot13(char *a)
+{
+	char e[52] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+			'j', 'k', 'l', 'm',
+			 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+			 'w', 'x', 'y', 'z',
+			 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+			 'J', 'K', 'L', 'M',
+			 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+			 'W', 'X', 'Y', 'Z' };
+	char l[52] = { 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+			 'w', 'x', 'y', 'z',
+			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+			 'j', 'k', 'l', 'm',
+			'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+			 'W', 'X', 'Y', 'Z',
+			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+			 'J', 'K', 'L', 'M'};
+	int i;
+	char *p;
+
+	p = a;
+
+	while (*p != '\0')
+	{
+
+		for (i = 0; i < 52; i++)
+		{
+			if (*p == e[i])
+				*p = l[i];
+		}
+		p++;
+	}
+	return (a);
+}
