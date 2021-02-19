78cc45cc351e29171df769b559bdde5e4ecf8427
Simon Sun <sunmingyu.97@gmail.com>
2021-02-19 04:05:24 +0000

    betty check

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,14 +1,13 @@
 /**
- * cap_string - change the word's first lowercase to upper case
  * _ismark - see if the char is a mark or not
  * @b: char type
- * @s: type char *
- * Return: char *.
+ * Return: int.
  */
 int _ismark(char b)
 {
 	int i;
 	char array[14] = { ',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\t', '\n', ' ', '\0' };
+
 	for (i = 0; i < 14; i++)
 	{
 		if (b == array[i])
@@ -19,28 +18,24 @@ int _ismark(char b)
 
 	return (1);
 }
-
+/**
+ * cap_string - change the word's first lowercase to upper case
+ * @s: type char *
+ * Return: char *.
+ */
 
 
 char *cap_string(char *s)
 {
 	char *a, *c;
 
-	c = s;	
 	a = s;
-	while (*c != '\0')
-	{
-		if (*c == '\t')
-			*c = ' ';
-		c++;
-	}
-
 	while (*a != '\0')
 	{
 		if (_ismark(*a))
 		{
 			if (*(a + 1) >= 'a' && *(a + 1) <= 'z')
-				*(a+1) = *(a+1) - 32;
+				*(a + 1) = *(a + 1) - 32;
 		}
 		a++;
 	}
