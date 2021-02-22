fb40c4668bab1e17b66f94eabc2517baec4a5ba1
Simon Sun <sunmingyu.97@gmail.com>
2021-02-22 06:44:15 +0000

    first push

---
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * print_buffer - print the buffer
+ * @b: type char*
+ * @size: type int
+ */
+void print_buffer(char *b, int size)
+{
+	char *p;
+	char *q;
+	int i, j, a;
+
+	p = b;
+	q = b;
+	for (i = 0, a = 0; i < size / 10 + 1; i++, a = a + 10)
+	{
+		printf("%08x:", a);
+
+		for (j = 0; j < 10; j++, p++)
+		{
+			if (j % 2 == 0)
+				printf(" ");
+
+			if (i * 10 + j > size - 1)
+				printf("  ");
+			else
+				printf("%02x", *p);
+			if (j % 9 == 0)
+				printf(" ");
+		}
+		for (j = 0; j < 10; j++, q++)
+		{
+			if (*q >= 32 && *q <= 122)
+				printf("%c", *q);
+			else
+				printf(".");
+			if (i * 10 + j > size - 2)
+				break;
+		}
+		printf("\n");
+	}
+}
