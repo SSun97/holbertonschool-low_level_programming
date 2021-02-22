77f2a333f9350712beae8987cb31a70787635180
Simon Sun <sunmingyu.97@gmail.com>
2021-02-22 07:23:23 +0000

    return 0 if size = 0

--- 0x06-pointers_arrays_strings/103-print_buffer.c
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -6,37 +6,38 @@
  */
 void print_buffer(char *b, int size)
 {
-	char *p;
-	char *q;
+	char *p, *q;
 	int i, j, a;
 
-	p = b;
-	q = b;
-	for (i = 0, a = 0; i < (size - 1) / 10 + 1; i++, a = a + 10)
-	{
-		printf("%08x:", a);
-
-		for (j = 0; j < 10; j++, p++)
-		{
-			if (j % 2 == 0)
-				printf(" ");
-
-			if (i * 10 + j > size - 1)
-				printf("  ");
-			else
-				printf("%02x", *p);
-		}
-		printf(" ");
-
-		for (j = 0; j < 10; j++, q++)
-		{
-			if (*q >= 32 && *q <= 122)
-				printf("%c", *q);
-			else
-				printf(".");
-			if (i * 10 + j > size - 2)
-				break;
-		}
+	if (size <= 0)
 		printf("\n");
+	else
+	{
+		p = b;
+		q = b;
+		for (i = 0, a = 0; i <= (size - 1) / 10; i++, a = a + 10)
+		{
+			printf("%08x:", a);
+			for (j = 0; j < 10; j++, p++)
+			{
+				if (j % 2 == 0)
+					printf(" ");
+				if (i * 10 + j > size - 1)
+					printf("  ");
+				else
+					printf("%02x", *p);
+			}
+			printf(" ");
+			for (j = 0; j < 10; j++, q++)
+			{
+				if (*q >= 32 && *q <= 122)
+					printf("%c", *q);
+				else
+					printf(".");
+				if (i * 10 + j > size - 2)
+					break;
+			}
+			printf("\n");
+		}
 	}
 }
