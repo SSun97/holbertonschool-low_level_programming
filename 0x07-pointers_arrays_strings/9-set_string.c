ca8f78b33b4bac5d45ab79e82dd42ea35d7423fb
Simon Sun <sunmingyu.97@gmail.com>
2021-02-23 03:31:09 +0000

    first push

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,10 @@
+/**
+ * set_string - Write a function that sets the value of a pointer to a char.
+ * @s: type char **
+ * @to: type char *
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+
+}
