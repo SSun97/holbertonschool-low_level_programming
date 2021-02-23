fcc231c594b7b7a2ba8b4e750850b65f2e551fa8
Simon Sun <sunmingyu.97@gmail.com>
2021-02-23 05:11:38 +0000

    betty check

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,14 +9,15 @@ unsigned int _strspn(char *s, char *accept)
 {
 	char c, *p, *s1;
 
-	for (s1 = s, c = *s1; c != 0; s1++, c = *s1) {
-		for (p = accept; *p != 0; p++) {
-			if (c == *p) {
+	for (s1 = s, c = *s1; c != 0; s1++, c = *s1)
+	{
+		for (p = accept; *p != 0; p++)
+		{
+			if (c == *p)
 				goto next;
-			}
 		}
 		break;
-	next:;
+next:;
 	}
 	return (s1 - s);
 }
