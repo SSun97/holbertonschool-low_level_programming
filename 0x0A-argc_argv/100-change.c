ab08b6a16ea63f35d757c2b32b67e6c19184dca4
Simon Sun <sunmingyu.97@gmail.com>
2021-02-27 19:54:46 +0000

    first push

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print the number of arguments
+ * @argc: type int
+ * @argv: unused
+ * Return: type int
+*/
+int main(int argc, char *argv[])
+{
+	int r, n;
+	int c25, c10, c5, c2, c1;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (0);
+	}
+
+	r = atoi(argv[1]);
+	c25 = r / 25;
+	c10 = (r - (25 * c25)) / 10;
+	c5 = (r - (25 * c25) - (10 * c10)) / 5;
+	c2 = (r - (25 * c25) - (10 * c10) - (5 * c5)) / 2;
+	c1 = (r - (25 * c25) - (10 * c10) - (5 * c5)) % 2;
+	n = c25 + c10 + c5 + c2 + c1;
+
+	printf("%d\n", n);
+	return (0);
+
+
+}
