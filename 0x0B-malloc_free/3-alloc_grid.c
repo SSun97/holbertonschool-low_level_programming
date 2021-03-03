4c9a9e2678ef7d479cdb05b4488a223df432c7a9
Simon Sun <sunmingyu.97@gmail.com>
2021-03-03 05:46:15 +0000

    consider if p[i] == null

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,13 +12,15 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	p = malloc(sizeof(int *) * height);
+	p = (int **)malloc(sizeof(int *) * height);
 	if (p == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
-		p[i] = malloc(sizeof(int) * width);
+		p[i] = (int *)malloc(sizeof(int) * width);
+		if (p[i] == NULL)
+			return (NULL);
 		for (j = 0; j < width; j++)
 			p[i][j] = 0;
 	}
