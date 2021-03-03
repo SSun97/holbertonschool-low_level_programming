c0df9cfc19449d68093574a3ca9e98f2d8c9c636
Simon Sun <sunmingyu.97@gmail.com>
2021-03-03 04:44:46 +0000

    first push

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+/**
+ * alloc_grid - check the code for Holberton School students.
+ * @width: type int
+ * @height: type int
+ * Return: int **.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **p;
+	int i, j;
+
+	p = (int **)malloc(sizeof(int) * height);
+
+	for (i = 0; i < height; i++)
+	{
+		p[i] = (int *)malloc(sizeof(int) * width);
+		for (j = 0; j < width; j++)
+			p[i][j] = 0;
+	}
+	return (p);
+
+}
+
