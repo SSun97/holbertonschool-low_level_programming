01f97aa820378a9a216ef385de2aa6746eb5c483
Simon Sun <sunmingyu.97@gmail.com>
2021-03-04 21:13:59 +0000

    first push

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+/**
+ * _calloc - open a space and put 0 to every block
+ * @nmemb: type unsigned int
+ * @size: type unsigned int
+ *
+ * Return: a pointer, otherwise NULL.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		*(p + i * size) = 0;
+	return (p);
+
+}
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -3,6 +3,8 @@
 void *malloc_checked(unsigned int b);
 int _putchar(char c);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+int *array_range(int min, int max);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 
 
