08bd0eb09262762ce51f65359397d47c738dc723
Simon Sun <sunmingyu.97@gmail.com>
2021-03-05 15:24:03 +0000

    exit 98

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,7 +9,7 @@ void *malloc_checked(unsigned int b)
 	void *p = malloc(b);
 
 	if (p == NULL)
-		exit(0);
+		exit(98);
 	return (p);
 
 
---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,21 @@
+/**
+ *imple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	int *p;
+
+	 bn 
+
+
+
+
+
+
+	return (p);
+}
