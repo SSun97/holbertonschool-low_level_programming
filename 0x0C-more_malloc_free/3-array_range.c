fab6e6af0cacd48096901ccec708adc43d38cd5b
Simon Sun <sunmingyu.97@gmail.com>
2021-03-05 00:31:00 +0000

    add code for min == max

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,11 +13,22 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	p = malloc((max - min + 1) * sizeof(int));
-	if (p == NULL)
-		return (NULL);
-	for (i = 0; i <= max - min + 1; i++)
-		*(p + i) = min + i;
+	else if (min < max)
+	{
+		p = malloc((max - min + 1) * sizeof(int));
+		if (p == NULL)
+			return (NULL);
+		for (i = 0; i <= max - min + 1; i++)
+			*(p + i) = min + i;
+	}
+	else
+	{
+		p = malloc(2 * sizeof(int));
+		if (p == NULL)
+			return (NULL);
+		*p = min;
+		*(p + 1) = min;
+	}
 
 	return (p);
 }
