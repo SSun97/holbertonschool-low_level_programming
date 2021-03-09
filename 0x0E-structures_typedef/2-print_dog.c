9f7a58c9eada75a6295794a17efd1c823ede254f
Simon Sun <sunmingyu.97@gmail.com>
2021-03-09 15:05:13 +0000

    correct name -> age

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,8 +14,8 @@ void print_dog(struct dog *d)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
-/* if ((*d).age== (float)NULL) printf("Age: (nil)\n"); else */
-		printf("Name: %f\n", d->age);
+
+		printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 	else
