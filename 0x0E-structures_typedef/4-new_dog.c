f6f7ad06ddc62d7209050327ebb788aaa85a0e04
Simon Sun <sunmingyu.97@gmail.com>
2021-03-09 05:27:21 +0000

    use malloc in fuction

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - check the code for Holberton School students.
+ * @name: type char *
+ * @age: type float
+ * @owner: type char *
+ * Return: dog_t *.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p;
+
+	p = malloc(sizeof(dog_t));
+	if (p == NULL)
+		return (NULL);
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+	return (p);
+
+}
