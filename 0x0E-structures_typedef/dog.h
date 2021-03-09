01a576206f708e95d3206e3952e08b7fd45e5d7b
Simon Sun <sunmingyu.97@gmail.com>
2021-03-09 01:58:38 +0000

    add comment

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
+
 struct dog
 {
 	char *name;
