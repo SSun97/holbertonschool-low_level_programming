1fadb32f73eb3ed7671ef3b383d7612bf0b6dc93
Simon Sun <sunmingyu.97@gmail.com>
2021-03-10 00:25:00 +0000

    adding malloc for store the string

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
-
+char *_strdup(char *str);
 /**
  * new_dog - check the code for Holberton School students.
  * @name: type char *
@@ -15,9 +15,44 @@ dog_t *new_dog(char *name, float age, char *owner)
 	p = malloc(sizeof(dog_t));
 	if (p == NULL)
 		return (NULL);
-	p->name = name;
+	p->name = _strdup(name);
+	if (p->name == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
 	p->age = age;
-	p->owner = owner;
+	p->owner = _strdup(owner);
+	if (p->owner == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	return (p);
+}
 
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory.
+ * @str: tyoe char *
+ * Return: pointer p, otherwith NULL.
+ */
+char *_strdup(char *str)
+{
+	int i;
+	char *p, *s;
+	int len;
+
+	if (str == NULL)
+		return (NULL);
+
+	s = str;
+	for (len = 0; *s != '\0'; len++)
+		s++;
+
+	p = malloc((len + 1) * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i <= len; i++)
+		p[i] = str[i];
 	return (p);
 }
