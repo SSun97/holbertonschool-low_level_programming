a9fbcde0bbef29bd96b69832405e1dcc4cba25e0
Simon Sun <sunmingyu.97@gmail.com>
2021-03-11 21:40:57 +0000

    push

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,7 +12,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *tmp;
 
-	if (*separator == '\0')
+	if (separator == NULL)
 		return;
 	va_start(p, n);
 	for (i = 0; i < n; i++)
