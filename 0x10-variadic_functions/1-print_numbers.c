409945e4fa09de049dbb91a06365cc314d16636a
Simon Sun <sunmingyu.97@gmail.com>
2021-03-11 20:45:51 +0000

    betty check

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,7 +14,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i = 0;
 
 	if (separator == NULL)
-                return;
+		return;
 	va_start(p, n);
 	for (i = 0; i < n; i++)
 	{
