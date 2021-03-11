becf31d2b0c66af68643242bcaffefc42de970f9
Simon Sun <sunmingyu.97@gmail.com>
2021-03-11 21:38:35 +0000

    first push

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,2 +1,32 @@
 #include <stdio.h>
 #include <stdarg.h>
+/**
+ * print_strings - print string
+ * @separator: tyoe const char
+ * @n: unsigned const int
+ *
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list p;
+	unsigned int i;
+	char *tmp;
+
+	if (*separator == '\0')
+		return;
+	va_start(p, n);
+	for (i = 0; i < n; i++)
+	{
+		tmp = va_arg(p, char *);
+
+		if (*tmp == '\0')
+			printf("(nil)");
+		else
+			printf("%s", tmp);
+		if (i < (n - 1))
+			printf("%s", separator);
+	}
+	va_end(p);
+	printf("\n");
+
+}
