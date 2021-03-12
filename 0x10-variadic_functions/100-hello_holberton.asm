828033f8189f29cc227e53ba798613f805f2a206
Simon Sun <sunmingyu.97@gmail.com>
2021-03-12 04:18:56 +0000

    _start conflict

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,26 +1,16 @@
-%define sys_write 1
-%define stdout 1
-
-%define sys_exit 60
-%define success 0
-
-%define nl 10
-
 section .data
 
-    message db "Hello, Holberton", nl
-    message_len equ $-message
+	message db "Hello, Holberton", 10
 
 section .text
+global main
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, message
+	mov rdx, 17
+	syscall
 
-global _start
-_start:
-    mov rax, sys_write
-    mov rdi, stdout
-    mov rsi, message
-    mov rdx, message_len
-    syscall
-
-    mov rax, sys_exit
-    mov rdi, success
-    syscall
+	mov rax, 60
+	mov rdi, 0
+	syscall
