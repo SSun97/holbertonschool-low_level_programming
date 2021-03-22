aa195468f9690870fce82a4a1cdc4ace9664912e
Simon Sun <sunmingyu.97@gmail.com>
2021-03-22 14:32:20 +0000

    first push

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - free the memory of list start from head
+ * @head: pointer to the starting point of a link list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *p = *head;
+
+	while (p)
+	{
+		p = p->next;
+		free(*head);
+		*head = p;
+	}
+	*head = NULL;
+}
