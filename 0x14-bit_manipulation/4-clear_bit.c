e0347b1a00093b79a28116b4d5b1bf9cfe29f962
Simon Sun <sunmingyu.97@gmail.com>
2021-03-25 20:12:15 +0000

    first push

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - set the index bit to 1
+ * @n: pointer to an unsigned long int
+ * @index: unsigned int
+ * Return: integer.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int a = 1;
+
+	if (index > 64)
+		return (-1);
+
+	*n = (*n & (~(a << index)));
+
+	return (1);
+
+
+
+
+}
