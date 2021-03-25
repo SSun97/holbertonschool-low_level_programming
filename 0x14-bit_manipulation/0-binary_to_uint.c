7d1b9b72bb4fbd26c149b4d79fb068d1b045a162
Simon Sun <sunmingyu.97@gmail.com>
2021-03-25 17:04:52 +0000

    solve the NULL string issue

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "holberton.h"
 /**
  * binary_to_uint - convert a string to integer
@@ -8,6 +9,8 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i = 0, j = 0, deci_num = 0;
 
+	if (b == NULL)
+		return (0);
 	while (b[i] != '\0')
 	{
 		if (b[i] != '1' && b[i] != '0')
