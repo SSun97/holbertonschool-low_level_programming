2c9a276fe3b654c22c28e8743decdb1f5f371475
Simon Sun <sunmingyu.97@gmail.com>
2021-03-25 21:26:01 +0000

    first push

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * flip_bits - returns the number of bits you would need to flip
+ * @n: unsigned long int
+ * @m: unsigned long int
+ * Return: unsigned int.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int i, z = 0;
+	unsigned long int xor, a = 1;
+
+	xor = n ^ m;
+	for (i = 0; i < 63; i++)
+	{
+		if ((xor & (a << i)) > 0)
+			z++;
+	}
+	return (z);
+}
