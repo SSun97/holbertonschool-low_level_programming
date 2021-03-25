9eb4a4fead7b5fffd67251d03c331ac74879275a
Simon Sun <sunmingyu.97@gmail.com>
2021-03-25 21:34:58 +0000

    delete the redundant code

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,9 +10,6 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int i, z = 0;
 	unsigned long int xor, a = 1;
 
-	if (n == m)
-		return (0);
-
 	xor = n ^ m;
 	for (i = 0; i < 64; i++)
 	{
