e05b4116e7afb809ff63b694bec3f05a8e232289
Simon Sun <sunmingyu.97@gmail.com>
2021-03-25 00:58:49 +0000

    solve the NULL issue

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,15 +10,16 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *p = *head;
+	listint_t *p;
 
-	if (*head == NULL)
-		return;
-	while (p)
+	if (head != NULL)
 	{
-		p = p->next;
-		free(*head);
-		*head = p;
+		while (*head != NULL)
+		{
+			p = *head;
+			*head = (*head)->next;
+			free(p);
+		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
