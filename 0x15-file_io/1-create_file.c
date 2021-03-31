8aa6724bfe8bb1437820fb5f1ca9d5eafc24a324
Simon Sun <sunmingyu.97@gmail.com>
2021-03-31 16:24:13 +0000

    check NULL issue

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd = 0, i = 0, j = 0;
 
-	if (filename == NULL)
+	if (filename == NULL || text_content == NULL)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
