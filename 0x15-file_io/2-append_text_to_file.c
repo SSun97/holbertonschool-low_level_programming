1887ff7e1b2a7a13ca3a95b91a895409bef317a8
Simon Sun <sunmingyu.97@gmail.com>
2021-03-31 01:33:08 +0000

    first push

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * append_text_to_file - create a file and write in text content
+ * @filename: the file to be opened or create
+ * @text_content: char *
+ * Return: Always 0.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd = 0, i = 0, j = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_APPEND);
+		if (fd == -1)
+			return (-1);
+	while (text_content[i])
+	{
+		i++;
+	}
+	if (i == 0)
+		return (1);
+	j = write(fd, text_content, i);
+		if (j == -1)
+			return (0);
+
+	close(fd);
+	return (1);
+}
