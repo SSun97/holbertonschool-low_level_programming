6f8161ffe09f8d2b3f765e22a37b8852a07587db
Simon Sun <sunmingyu.97@gmail.com>
2021-03-31 01:34:57 +0000

    return -1 for failure

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,11 +28,11 @@ int create_file(const char *filename, char *text_content)
 	{
 	j = write(fd, '\0', 1);
 		if (j == -1)
-			return (0);
+			return (-1);
 	}
 	j = write(fd, text_content, i);
 		if (j == -1)
-			return (0);
+			return (-1);
 
 	close(fd);
 	return (1);
