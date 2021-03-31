f9850d38923cafe797499e77bae7499630352bed
Simon Sun <sunmingyu.97@gmail.com>
2021-03-31 18:20:39 +0000

    check the NULL pointer passed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,12 +14,17 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd = 0, i = 0, j = 0;
 
-	if (filename == NULL || *text_content == '\0')
+	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 		if (fd == -1)
 			return (-1);
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
 	while (text_content[i])
 	{
 		i++;
