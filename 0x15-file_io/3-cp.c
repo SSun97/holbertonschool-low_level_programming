c0a62645765f0ee39d70671da5b9f2b9c7c1741a
Simon Sun <sunmingyu.97@gmail.com>
2021-03-31 04:17:18 +0000

    combined into 1 function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,26 +4,31 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 /**
- * cp_file_to_file - copy file from to
- * @filefrom: source file
- * @fileto: target file
+ * main - copy file from to
+ * @ac: number of arguments
+ * @av: pointer to the arguments
  * Return: 0.
  */
-int cp_file_to_file(const char *filefrom, const char *fileto)
+int main(int ac, char *av[])
 {
 	int fd_from, fd_to, i = 1, j = 1;
 	char buf[1024] = {'\0'};
 
-	fd_to = open(fileto, O_CREAT | O_RDWR | O_TRUNC, 0664);
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd_to = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
 		if (fd_to == -1)
 		{
-			dprintf(2, "Error: Can't write to %s\n", fileto);
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
-	fd_from = open(filefrom, O_RDONLY);
+	fd_from = open(av[1], O_RDONLY);
 		if (fd_from == -1)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", filefrom);
+			dprintf(2, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
 		}
 	while (i)
@@ -40,24 +45,6 @@ int cp_file_to_file(const char *filefrom, const char *fileto)
 			dprintf(2, "Error: Can't close fd -1\n");
 			exit(100);
 		}
-	return (0);
-}
 
-/**
- * main - copy file from to
- * @ac: number of arguments
- * @av: pointer to the arguments
- * Return: 0.
- */
-int main(int ac, char **av)
-{
-
-	if (ac != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	cp_file_to_file(av[1], av[2]);
 	return (0);
 }
