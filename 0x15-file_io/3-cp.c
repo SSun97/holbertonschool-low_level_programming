60e89e82f94634f069cd644ac00810667abf8dbe
Simon Sun <sunmingyu.97@gmail.com>
2021-03-31 19:04:04 +0000

    follow the POINX

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -22,13 +22,13 @@ int main(int ac, char *av[])
 	fd_to = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
 		if (fd_to == -1)
 		{
-			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
 	fd_from = open(av[1], O_RDONLY);
 		if (fd_from == -1)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", av[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
 		}
 	while (i)
@@ -42,7 +42,7 @@ int main(int ac, char *av[])
 	j = close(fd_to);
 		if (i == -1 || j == -1)
 		{
-			dprintf(2, "Error: Can't close fd -1\n");
+			dprintf(STDERR_FILENO, "Error: Can't close fd -1\n");
 			exit(100);
 		}
 
