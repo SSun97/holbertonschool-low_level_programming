a1e027bbd4cc11ca1c9e45d54c1e7869031ee436
Simon Sun <sunmingyu.97@gmail.com>
2021-03-31 01:42:27 +0000

    return -1 for failure

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -28,7 +28,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (1);
 	j = write(fd, text_content, i);
 		if (j == -1)
-			return (0);
+			return (-1);
 
 	close(fd);
 	return (1);
