c3209fda0f043236a0a3987a0dd01b2d5ae32a4c
Simon Sun <sunmingyu.97@gmail.com>
2021-05-02 15:47:18 +0000

    add doc

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+''' The function that calculate the perimeter of an island '''
+
+
 def island_perimeter(grid):
     ''' The function that calculate the perimeter of an island '''
     perimeter = 0
