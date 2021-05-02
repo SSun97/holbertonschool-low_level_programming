347a04ab651cba86e46034714fcd88840df8e836
Simon Sun <sunmingyu.97@gmail.com>
2021-05-02 16:10:15 +0000

    add none situation code

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,19 +4,23 @@
 
 def island_perimeter(grid):
     ''' The function that calculate the perimeter of an island '''
-    perimeter = 0
-    adjacents = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == 0:
-                continue
-            origin = 4
-            for adjacent in adjacents:
-                new_x = i + adjacent[0]
-                new_y = j + adjacent[1]
-                if 0 < new_x < len(grid) and 0 < new_y < len(grid[i]) and\
-                   grid[new_x][new_y] == 1:
-                    origin -= 1
-            perimeter += origin
 
-    return perimeter
+    perimeter = 0
+    if grid:
+        adjacents = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 0:
+                    continue
+                origin = 4
+                for adjacent in adjacents:
+                    new_x = i + adjacent[0]
+                    new_y = j + adjacent[1]
+                    if 0 < new_x < len(grid) and 0 < new_y < len(grid[i]) and\
+                       grid[new_x][new_y] == 1:
+                        origin -= 1
+                perimeter += origin
+
+        return perimeter
+    else:
+        return 0
