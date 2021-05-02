dba581696fd78cac722e03e8eb82dfa8634dc5e2
Simon Sun <sunmingyu.97@gmail.com>
2021-05-02 22:38:09 +0000

    first push

---
+++ 0x18-dynamic_libraries/100-operations.h
@@ -0,0 +1,10 @@
+#ifndef CLAC_H
+#define CLAC_H
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+#endif
---
+++ 0x18-dynamic_libraries/funcs.c
@@ -0,0 +1,50 @@
+/**
+ * add - add two integer
+ * @a: tyoe int
+ * @b: tyoe int
+ * Return: result
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - sub two integer
+ * @a: tyoe int
+ * @b: tyoe int
+ * Return: result
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multipal two integer
+ * @a: tyoe int
+ * @b: tyoe int
+ * Return: result
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - divide two integer
+ * @a: tyoe int
+ * @b: tyoe int
+ * Return: result
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - module two integer
+ * @a: tyoe int
+ * @b: tyoe int
+ * Return: result
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
