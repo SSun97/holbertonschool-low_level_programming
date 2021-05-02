eb4d9bc9c343c93a4785359d21b6810e9e221117
Simon Sun <sunmingyu.97@gmail.com>
2021-05-02 02:51:12 +0000

    first first push

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    perimeter = 0
+    adjacents = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 0:
+                continue
+            origin = 4
+            for adjacent in adjacents:
+                new_x = i + adjacent[0]
+                new_y = j + adjacent[1]
+                if 0 < new_x < len(grid) and 0 < new_y < len(grid[i]) and\
+                   grid[new_x][new_y] == 1:
+                    origin -= 1
+            perimeter += origin
+
+    return perimeter
